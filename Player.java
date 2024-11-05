3558baf7c41886db8b24aa1dd183d4a7533297cc
JW <166519339+Scientific-Railgun@users.noreply.github.com>
2024-11-05 15:29:57 -0500

    updated player

--- Player.java
+++ Player.java
@@ -1,4 +1,33 @@
+package TreasureQuest;
 
 public class Player {
-
+	private static int health = 1;
+	private static int power = 100;
+	private static int points = 0;
+	
+	private static int getLife() {
+		return health;
+	}
+	
+	private static int getPower() {
+		return power;
+	}
+	
+	private static int getPoints() {
+		return points;
+	}
+	
+	public void update(int type, int val) {
+		switch(type) {
+			case 1: 
+				health = val;
+				break;
+			case 2: 
+				power = val;
+				break;
+			case 3:
+				points = val;
+				break;
+		}
+	}
 }
