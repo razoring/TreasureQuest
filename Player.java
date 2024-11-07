6eab3c6d681c4fd30a202e2248ebb499d5c0daed
JW <166519339+Scientific-Railgun@users.noreply.github.com>
2024-11-07 14:23:35 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,39 +3,36 @@ package TreasureQuest;
 import static java.lang.System.out;
 
 public class Player {
-	private static int lives = 1;
-	private static int lvl = 0;
-	private static int points = 0;
+	private static int lives;
+	private static int power;
+	private static int points;
+	
+	public Player() {
+		lives = 1;
+		power = 100;
+		points = 0;
+	}
 	
 	private static int getLife() {
 		return lives;
 	}
 	
 	private static int getPower() {
-		return lvl;
+		return power;
 	}
 	
 	private static int getPoints() {
 		return points;
 	}
 	
-	public void update(int type, int val) {
+	public void update(String type, int val) {
 		switch(type) {
-			case 1: 
+			case "Life": 
 				lives = val;
 				break;
-			case 2: 
-				lvl = val;
-				break;
-			case 3:
+			case "Points":
 				points = val;
 				break;
 		}
-	}
-	
-	public Player() {
-		lives = 1;
-		lvl = 100;
-		points = 0;
 	}
 }
